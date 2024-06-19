1e7c14abe6d412b12577bf4d937ccaeb0b28ffe1
GGonzoAD <158357205+GGonzoAD@users.noreply.github.com>
2024-06-19 13:45:54 -0700

    message

--- Script.js
+++ Script.js
@@ -1,4 +1,23 @@
-for(var i = 0; 1 < 16; i++){
-    var div = document.createElement("div");
-    document.body.appendChild("div");
+var container = document.getElementById("container");
+
+function createColumn(size){
+    for(let i = 0; i < size; i++){
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("column");
+        newDiv.style.width = 600/size + "px";
+        document.getElementById("grid-container").appendChild(newDiv);
+        createRow(size);
+    }
 }
+
+function createRow(size){
+    for(let i = 0; i < size; i++){
+        let newRow = document.createElement("div");
+        newRow.classList.add("cell-row");
+        newRow.style.width = 600/size + "px";
+        document.getElementById("grid-container").appendChild(newRow);
+        createRow(size);
+    }
+}
+
+createColumn(16);
