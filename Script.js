9abf7567e18324921137654ec4880f3c9c4f5f03
GGonzoAD <158357205+GGonzoAD@users.noreply.github.com>
2024-06-20 16:19:43 -0700

    message

--- Script.js
+++ Script.js
@@ -2,7 +2,6 @@ function createColumn(size) {
     for (let i = 0; i < size; i++) {
         let newDiv = document.createElement("div");
         newDiv.classList.add("column");
-        newDiv.style.width = 600 / size + "px";
         console.log(newDiv);
         document.getElementById("grid-container").appendChild(newDiv);
         
@@ -11,7 +10,6 @@ function createColumn(size) {
             for (let j = 0; j < size; j++) {
                 let newRow = document.createElement("div");
                 newRow.classList.add("cell");
-                newRow.style.height = 600 / size + "px";
                 console.log(newRow);
                 let columns = document.querySelectorAll(".column");
                 columns[i].appendChild(newRow);
