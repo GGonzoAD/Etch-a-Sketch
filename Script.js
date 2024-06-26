884e881a6f5ca2d38c4c229fc17689ef5d6ecdbc
GGonzoAD <158357205+GGonzoAD@users.noreply.github.com>
2024-06-26 15:18:07 -0700

    message

--- Script.js
+++ Script.js
@@ -27,7 +27,7 @@ function hoverColor(){
 
 
     function GridReset(){
-    let Size = document.querySelectorAll("size");
+    let Size = document.querySelectorAll('size');
     let Items = document.querySelectorAll('.cell');
     Items.forEach(Item => {
       Item.style.backgroundColor = 'rgb(54, 52, 52)';
@@ -35,6 +35,7 @@ function hoverColor(){
     var answer = prompt("What grid size would you like?");
     parseInt(answer,10);
     Size = answer;
+    createColumn(Size);
     }
 
-createColumn(16);
+
